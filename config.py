bc77a1693b424c48e18275c46c767c9601180ef2
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2022-08-28 08:51:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-AKI_MONGO_HOST = os.environ.get('aki_mongo_host', "")
-BOT_TOKEN = os.environ.get('bot_token', "")
+AKI_MONGO_HOST = "mongodb+srv://Ganyu:1xG5EJkbqf5PyOuu@ganyu.o7poa.mongodb.net/?retryWrites=true&w=majority"
+BOT_TOKEN = "5522405803:AAGJLQLDobdRgvJSR9HGTcRVuNUGa58ECQA"
