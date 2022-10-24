fa8193f273256ac36b64d43bbcb90080f281da95
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2022-10-24 14:37:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 AKI_MONGO_HOST = "mongodb+srv://Ganyu:1xG5EJkbqf5PyOuu@ganyu.o7poa.mongodb.net/?retryWrites=true&w=majority"
-BOT_TOKEN = "5522405803:AAGJLQLDobdRgvJSR9HGTcRVuNUGa58ECQA"
+BOT_TOKEN = "5738549546:AAFiRIySWo_XPRDI5JYv2PxlOzLs_Aw2K9M"
